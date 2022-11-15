19713f9e109391270da367c3ca8a49c8a9f4dc21
Bob Kagia <rwgatonga@gmail.com>
2022-11-15 12:47:15 +0900

    initial

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className='sidebar__header'>
+        <div className='sidebar__headerRight'>r</div>
+        <div className='sidebar__headerLeft'>l</div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-GiDUxvb8p4A2P6xGSw3JR_IaP2kgXbg',
+  authDomain: 'whatsapp-clone-d6d93.firebaseapp.com',
+  projectId: 'whatsapp-clone-d6d93',
+  storageBucket: 'whatsapp-clone-d6d93.appspot.com',
+  messagingSenderId: '955399446608',
+  appId: '1:955399446608:web:b2f33cbc3d715f16927155',
+};
